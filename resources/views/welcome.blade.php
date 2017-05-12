1122e614f1c87b0dffa5a2ad014c0d2e6aa60d7b
stamlercas <stamlercas@yahoo.com>
2017-05-12 15:10:47 -0400

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <title>Laravel</title>
+        <title>Pittsburgh Steelers Internship Project</title>
 
         <link href="{{ URL::asset('css/app.css') }}" rel="stylesheet" type="text/css" />
         <script src="{{ URL::asset('js/app.js') }}"></script>
